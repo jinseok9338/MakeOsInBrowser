30748566bda17641130f7da038b9b04086e10f2f
jinseok9338 <jinseok9338@gmail.com>
2021-07-12 10:39:21 +0000

    Added TypeScript support

---
+++ os-in-browser/pages/_app.tsx
@@ -0,0 +1,10 @@
+import 'styles/globals.scss' //File import 
+
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+export default function MyApp({ Component, pageProps }:AppProps) {
+  return <Component {...pageProps} />
+}
+
+
