e900077676a4fd1af588b12825698452a00c457d
jinseok9338 <jinseok9338@gmail.com>
2021-07-17 09:50:05 +0000

    Added jest and eslint for jest

--- os-in-browser/__test__/pages/index.spec.tsx
+++ os-in-browser/__test__/pages/index.spec.tsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { render } from '@testing-library/react';
 
 import Home from '../../pages/index';
@@ -5,5 +9,5 @@ import Home from '../../pages/index';
 test('renders deploy link', () => {
   const { getByText } = render(<Home />);
   const HelloWorldElement = getByText('Hello World');
-  // expect(HelloWorldElement).toBeInTheDocument();
+  expect(HelloWorldElement).toBeInTheDocument();
 });
--- os-in-browser/jest.config.js
+++ os-in-browser/jest.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
-
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
-  ],
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js'
-  }
+  moduleDirectories: ['.', 'node_modules'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
