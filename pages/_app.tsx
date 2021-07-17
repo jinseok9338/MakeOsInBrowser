9628f663902d46171907e36b7a25a68568e0414b
jinseok9338 <jinseok9338@gmail.com>
2021-07-17 13:25:08 +0000

    added Meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,15 @@ import '../styles/globals.scss'; // File import
 // import App from "next/app";
 import type { AppProps /* , AppContext */ } from 'next/app';
 import Head from 'next/head';
-import { name } from 'package.json';
+import _package from 'package.json';
 import type { ReactElement } from 'react';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
       <Head>
-        <title> {name}</title>
+        <title> {_package.name}</title>
+        <meta name="description" content={_package.description} />
       </Head>
       <Component {...pageProps} />
     </>
