be2687e61152c3b483137a055108d80e8ecd47c5
jinseok9338 <jinseok9338@gmail.com>
2021-07-17 14:30:19 +0000

    Fixed test tyled components

--- __test__/pages/index.spec.tsx
+++ __test__/pages/index.spec.tsx
@@ -1,13 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-
 import { render } from '@testing-library/react';
+import Index from 'pages/index';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from 'themes/default.json';
 
-import Home from '../../pages/index';
+test('renders index page', () => {
+  const { getByText } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <Index />
+    </ThemeProvider>
+  );
+  const helloWorldElement = getByText('Hello, world!');
 
-test('renders deploy link', () => {
-  const { getByText } = render(<Home />);
-  const HelloWorldElement = getByText('Hello World');
-  expect(HelloWorldElement).toBeInTheDocument();
+  expect(helloWorldElement).toBeInTheDocument();
 });
