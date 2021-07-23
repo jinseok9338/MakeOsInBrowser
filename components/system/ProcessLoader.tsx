a7e51534368bc09e5dd8c02996a52abdf765f746
jinseok9338 <jinseok9338@gmail.com>
2021-07-23 11:09:30 +0000

    Working on RenderProcess Component

--- components/system/ProcessLoader.tsx
+++ components/system/ProcessLoader.tsx
@@ -1,22 +1,30 @@
 import Window from 'components/system/Window';
 import { ProcessConsumer } from 'contexts/process';
 import type { FC } from 'react';
+import type { Process, Processes } from 'types/contexts/process';
 
 // DO I need key ={id} ? Add Linting rule to catch key required
 
+const RenderProcess: FC<Process> = ({ Component, hasWindow }) =>
+  hasWindow ? (
+    <Window>
+      <Component />
+    </Window>
+  ) : (
+    <Component />
+  );
+
+const RenderProcesses: FC<{ processes: Processes }> = ({ processes }) => (
+  <>
+    {Object.entries(processes).map(([id, { Component, hasWindow }]) => (
+      <RenderProcess key={id} Component={Component} hasWindow={hasWindow} />
+    ))}
+  </>
+);
+
 const ProcessLoader: FC = () => (
   <ProcessConsumer>
-    {({ processes }) =>
-      Object.entries(processes).map(([id, { Component, hasWindow }]) =>
-        hasWindow ? (
-          <Window key={id}>
-            <Component />
-          </Window>
-        ) : (
-          <Component key={id} />
-        )
-      )
-    }
+    {({ processes }) => <RenderProcesses processes={processes} />}
   </ProcessConsumer>
 );
 
