e1f54e7acf368e41cabf78d6dd178a24e2c9e779
jinseok9338 <jinseok9338@gmail.com>
2021-07-25 07:47:16 +0000

    Migrate context to factory

---
+++ contexts/contextFactory.tsx
@@ -0,0 +1,26 @@
+import type { FC } from 'react';
+import { createContext } from 'react';
+import type { ContextFactory } from 'types/contexts/contextFactory';
+
+const contextFactory: ContextFactory = (
+  initialContextState,
+  useContextState
+) => {
+  const { Consumer, Provider } = createContext(initialContextState);
+
+  const ProcessProvider: FC = ({ children }) => (
+    <Provider value={useContextState()}>{children}</Provider>
+  );
+
+  return { Consumer, Provider: ProcessProvider };
+};
+
+export default contextFactory;
+
+// const {Consumer, Provider} = contextFactory(
+
+// )
+
+// export default {
+//     Consumer as ProcessConsumer, Provider as ProcessProvider
+// }
