1cd549deadf1d1c8f2cfa626890ec28e190abe0f
jinseok9338 <jinseok9338@gmail.com>
2021-08-01 12:54:05 +0000

    Added type to button

--- components/system/Taskbar/StartButton.tsx
+++ components/system/Taskbar/StartButton.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react';
 import StyledStartButton from 'styles/components/system/StyledStartButton';
 
-const StartButton: FC = () => <StyledStartButton>s</StyledStartButton>;
+const StartButton: FC = () => (
+  <StyledStartButton type="button">s</StyledStartButton>
+);
 
 export default StartButton;
