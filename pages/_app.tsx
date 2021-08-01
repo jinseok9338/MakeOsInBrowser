2d304a16370ff4fcf880e8861a392f2e0c53bdc7
jinseok9338 <jinseok9338@gmail.com>
2021-08-01 13:16:11 +0000

    Moved metadata into session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,12 @@ import type { ReactElement } from 'react';
 import StyledApp from '../components/pages/styledApp'; // Move to environment
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => (
-  <>
-    <MetaData />
-    <SessionProvider>
-      <StyledApp>
-        <Component {...pageProps} />
-      </StyledApp>
-    </SessionProvider>
-  </>
+  <SessionProvider>
+    <StyledApp>
+      <MetaData />
+      <Component {...pageProps} />
+    </StyledApp>
+  </SessionProvider>
 );
 
 export default App;
