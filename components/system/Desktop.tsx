57a77054d873479b31ff65b265ef28f87e201ace
jinseok9338 <jinseok9338@gmail.com>
2021-08-02 10:17:34 +0000

    Removed Explicit role for main

--- components/system/Desktop.tsx
+++ components/system/Desktop.tsx
@@ -7,11 +7,7 @@ const Desktop: FC = ({ children }) => {
   const desktopRef = useRef<HTMLElement | null>(null);
 
   useWallpaper(desktopRef);
-  return (
-    <StyledDesktop ref={desktopRef} role="main">
-      {children}
-    </StyledDesktop>
-  );
+  return <StyledDesktop ref={desktopRef}>{children}</StyledDesktop>;
 };
 
 export default Desktop;
