0593e7b785ea6a0170fe85a43dcdd617b5112413
jinseok9338 <jinseok9338@gmail.com>
2021-08-07 10:42:48 +0000

    Simplify Render Process

--- components/system/Processes/RenderProcess.tsx
+++ components/system/Processes/RenderProcess.tsx
@@ -1,16 +1,16 @@
 import dynamic from 'next/dynamic';
-import type { FC } from 'react';
+import type { ComponentType, FC } from 'react';
 import type { Process } from 'types/contexts/process';
 
 const Window = dynamic(() => import('components/system/Window'));
 
-const RenderProcess = ({ Component, hasWindow }: Process): JSX.Element =>
-  hasWindow ? (
-    <Window>
-      <Component />
-    </Window>
-  ) : (
+const withWindow = (Component: ComponentType) => (
+  <Window>
     <Component />
-  );
+  </Window>
+);
+
+const RenderProcess = ({ Component, hasWindow }: Process): JSX.Element =>
+  hasWindow ? withWindow(Component) : <Component />;
 
 export default RenderProcess;
