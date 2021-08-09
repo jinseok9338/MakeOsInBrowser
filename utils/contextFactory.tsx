4f2df5f0d8e3afa8c8ecfb046a17c27d2bebfee1
jinseok9338 <jinseok9338@gmail.com>
2021-08-09 10:45:11 +0000

    export useContext from Factory

--- utils/contextFactory.tsx
+++ utils/contextFactory.tsx
@@ -1,22 +1,26 @@
 import type { FC } from 'react';
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
 type ContextFactory = <T>(
   initialContextState: T,
   useContextState: () => T
-) => { Consumer: React.Consumer<T>; Provider: React.FC };
+) => { Consumer: React.Consumer<T>; Provider: React.FC; useContext: () => T };
 
 const contextFactory: ContextFactory = (
   initialContextState,
   useContextState
 ) => {
-  const { Consumer, Provider } = createContext(initialContextState);
+  const Context = createContext(initialContextState);
 
   const ProcessProvider: FC = ({ children }) => (
-    <Provider value={useContextState()}>{children}</Provider>
+    <Context.Provider value={useContextState()}>{children}</Context.Provider>
   );
 
-  return { Consumer, Provider: ProcessProvider };
+  return {
+    Consumer: Context.Consumer,
+    Provider: ProcessProvider,
+    useContext: () => useContext(Context)
+  };
 };
 
 export default contextFactory;
